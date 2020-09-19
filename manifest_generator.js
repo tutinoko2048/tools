f6c977ff11a9e331266de3859f76df99b1b94e40
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2020-09-19 12:14:00 +0900

    Add files via upload

---
+++ manifest_generator.js
@@ -0,0 +1,60 @@
+function generateUuid() {
+    // https://github.com/GoogleChrome/chrome-platform-analytics/blob/master/src/internal/identifier.js
+    // const FORMAT: string = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
+    let chars = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".split("");
+    for (let i = 0, len = chars.length; i < len; i++) {
+        switch (chars[i]) {
+            case "x":
+                chars[i] = Math.floor(Math.random() * 16).toString(16);
+                break;
+            case "y":
+                chars[i] = (Math.floor(Math.random() * 4) + 8).toString(16);
+                break;
+        }
+    }
+    return chars.join("");
+}
+
+function func1() {
+    let manifest_name = document.getElementById("manifest_name").value;
+    let manifest_desc = document.getElementById("manifest_desc").value;
+    let manifest_ver = document.getElementById("manifest_ver").value;
+    // select要素を取得
+    let element = document.getElementById("manifest_type");
+
+    // 選択状態の項目の値を取得
+    let manifest_type = element.value;
+
+    let uuid1 = generateUuid();
+    let uuid2 = generateUuid();
+
+    let Datajson = {
+  "format_version": 1,
+  "header": {
+    "description": manifest_desc,
+    "name": manifest_name,
+    "uuid": uuid1,
+    "version": [manifest_ver]
+  },
+  "modules": [
+    {
+      "description": manifest_desc,
+      "type": manifest_type,
+      "uuid": uuid2,
+      "version": [manifest_ver]
+    }
+  ]
+};
+
+    let Encodejson = JSON.stringify(Datajson, null , 2);
+
+    document.getElementById("manifest_output").innerHTML = Encodejson;
+ 
+    
+    let textarea = document.getElementsByTagName("textarea")[0];
+    // 文字をすべて選択
+    textarea.select();
+    // コピー
+    document.execCommand("copy");
+    
+  }
