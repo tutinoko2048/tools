21ffa897fe1e1bfd0e1f5a277a8f009dac57010d
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2020-10-10 12:23:47 +0900

    拡大させないようにした

--- js/cps_tester.js
+++ js/cps_tester.js
@@ -4,3 +4,13 @@ function count() {
     document.getElementById("output").innerHTML = ++ click_count
     
   }
+
+/* ダブルタップによる拡大を禁止 */
+  var t = 0;
+  document.documentElement.addEventListener('touchend', function (e) {
+  var now = new Date().getTime();
+  if ((now - t) < 350){
+    e.preventDefault();
+  }
+  t = now;
+  }, false);
