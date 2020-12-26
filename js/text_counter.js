609de0feb7309d3f0102294b37cabfa99dab6a3c
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2020-12-27 07:31:18 +0900

    Add files via upload

--- js/text_counter.js
+++ js/text_counter.js
@@ -19,3 +19,52 @@ function func1() {
     document.execCommand("copy");
     
   }
+  
+  function text() {
+    min = window.prompt("min", "0");
+    max = window.prompt("max", "");
+    let text = document.getElementById("text").value;
+
+    for (min < max) {
+    let output = min++;
+    
+    }
+    
+    document.getElementById("output").innerHTML = output;
+    
+  }
+  
+  function disp(){
+
+	// 入力ダイアログを表示 ＋ 入力内容を user に代入
+	user =  window.prompt("ユーザー名を入力してください", "");
+
+	// 入力内容が tama の場合は example_tama.html にジャンプ
+	if(user == 'tama'){
+
+		location.href = "example_tama.html";
+
+	}
+
+	// 入力内容が hana の場合は example_hana.html にジャンプ
+	else if(user == 'hana'){
+
+		location.href = "example_hana.html";
+
+	}
+
+	// 入力内容が一致しない場合は警告ダイアログを表示
+	else if(user != "" && user != null){
+
+		window.alert(user + 'さんは登録されていません');
+
+	}
+
+	// 空の場合やキャンセルした場合は警告ダイアログを表示
+	else{
+
+		window.alert('キャンセルされました');
+
+	}
+
+}
