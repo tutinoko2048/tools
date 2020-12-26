b640952f4101f0da19a1427fef667b2d58334275
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2020-12-26 17:02:04 +0900

    Add files via upload

---
+++ js/text_counter.js
@@ -0,0 +1,21 @@
+function func1() {
+    let min = document.getElementById("min").value;
+    let max = document.getElementById("max").value;
+    let text = document.getElementById("text").value;
+
+
+    for (min < max) {
+    let output = min + 1
+    
+    }
+
+    
+    document.getElementById("output").innerHTML = output;
+    
+    let textarea = document.getElementsByTagName("textarea")[0];
+    // 文字をすべて選択
+    textarea.select();
+    // コピー
+    document.execCommand("copy");
+    
+  }
