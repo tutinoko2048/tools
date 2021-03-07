5c895d6a8666fd4f85b63faa796aa51dd98c7d64
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2021-03-07 07:36:42 +0000

    Update json_formatter.js

--- js/json_formatter.js
+++ js/json_formatter.js
@@ -1,24 +1,5 @@
 var inputfile = document.getElementById('inputfile')
 var image = document.getElementById('img')
- 
-inputfile.addEventListener("change", function(e) {
- 
- var file = e.target.files 
- var reader = new FileReader()
- reader.readAsDataURL(file[0])
- reader.onload = function() {
-   image.src = reader.result;
- }
- var input = document.querySelector('#inputfile').files[0];
-// 最後に、反映
-document.querySelector('#name').innerHTML = input.name;
-document.querySelector('#type').innerHTML = input.type;
-let size_small = Math.round(input.size / 1024) + 'KB';
-document.querySelector('#size').innerHTML = size_small;
-document.querySelector('#daytime').innerHTML = input.lastModifiedDate　;
-
- }, false)
-
 
 
 document.addEventListener('DOMContentLoaded',() => {
@@ -40,5 +21,23 @@ document.addEventListener('DOMContentLoaded',() => {
     var inputfile = document.querySelectorAll('input[name="fileupload"]'); //好きなようにDOMを取得してください
     input[0].files = event.dataTransfer.files; //取得したinput[type=file]にDropしたファイルを突っ込む
   });
-
 });
+
+ 
+inputfile.addEventListener("change", function(e) {
+ 
+ var file = e.target.files 
+ var reader = new FileReader()
+ reader.readAsDataURL(file[0])
+ reader.onload = function() {
+   image.src = reader.result;
+ }
+ var input = document.querySelector('#inputfile').files[0];
+// 最後に、反映
+document.querySelector('#name').innerHTML = input.name;
+document.querySelector('#type').innerHTML = input.type;
+let size_small = Math.round(input.size / 1024) + 'KB';
+document.querySelector('#size').innerHTML = size_small;
+document.querySelector('#daytime').innerHTML = input.lastModifiedDate　;
+
+ }, false)
