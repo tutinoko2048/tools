37b4298260d6b525df9a30506bd44d0ef57ab448
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2021-03-14 00:25:40 +0000

    Update json_formatter.js

--- js/json_formatter.js
+++ js/json_formatter.js
@@ -30,7 +30,7 @@ inputfile.addEventListener("change", function(e) {
  var reader = new FileReader()
  reader.readAsText(file[0])
  reader.onload = function() {
-   document.getElementById('output') = reader.result;
+   document.getElementById('output').value = reader.result;
  }
  var input = document.querySelector('#inputfile').files[0];
 // 最後に、反映
@@ -38,6 +38,5 @@ document.querySelector('#name').innerHTML = input.name;
 document.querySelector('#type').innerHTML = input.type;
 let size_small = Math.round(input.size / 1024) + 'KB';
 document.querySelector('#size').innerHTML = size_small;
-document.querySelector('#daytime').innerHTML = input.lastModifiedDate　;
 
  }, false)
