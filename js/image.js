18c4b5c48e1dba911ec90baef1fb2e17b94dcab5
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2021-07-30 23:35:00 +0900

    Update image.js

--- js/image.js
+++ js/image.js
@@ -1,11 +1,11 @@
-let input = document.getElementById('input');
-let button = document.getElementById('button');
+const input = document.getElementById('input');
+const button = document.getElementById('button');
 button.disabled = true;
-let width = document.getElementById('width');
+const width = document.getElementById('width');
 width.disabled = true;
-let height = document.getElementById('height');
+const height = document.getElementById('height');
 height.disabled = true;
-let download = document.getElementById('download');
+const download = document.getElementById('download');
 download.disabled = true;
 
 function preview(event) {
@@ -23,7 +23,7 @@ function preview(event) {
     var img = document.createElement("img");
     img.setAttribute("src", reader.result);
     img.setAttribute("id", "previewImage");
-    img.setAttribute("style", "width:80%;");
+    img.setAttribute("style", "width:400px;");
     preview.appendChild(img);
 
   };
@@ -116,11 +116,72 @@ button.addEventListener('click', function() {
       document.getElementById('outputCanvas').remove();
     }
     document.getElementById('output').appendChild(canvas);
-  }
-  download.disabled = false;
+
+    download.disabled = false;
+    var imgData = ctx.getImageData(0, 0, result.width, result.height);
+    console.log(imgData.data.length / 4)
+    console.log(result.width + ' ' + result.height)
+    // 16色カラーパレット
+    const palettes = ['E9ECEC', 'F07613', 'BD44B3', '3AAFD9', 'F8C627', '70B919', 'ED8DAC', '3E4447', '8E8E86', '158991', '792AAC', '35399D', '724728', '546D1B', 'A12722', '141519'];
+    // 各ピクセルの色情報設定
+    for (i = 0; i < result.width; i++) {
+      for (j = 0; j < result.height; j++) {
+
+        var rgb = {
+          r: imgData.data[j * 4 + i * imgData.width * 4],
+          g: imgData.data[1 + j * 4 + i * imgData.width * 4],
+          b: imgData.data[2 + j * 4 + i * imgData.width * 4]
+        }
+        var color = chooseColor(palettes, rgb);
+        var outRGB = convertToRGB(color);
+        // 赤成分
+        imgData.data[j * 4 + i * imgData.width * 4] = outRGB.r;
+        // 緑成分
+        imgData.data[1 + j * 4 + i * imgData.width * 4] = outRGB.g;
+        // 青成分
+        imgData.data[2 + j * 4 + i * imgData.width * 4] = outRGB.b;
+      }
+    }
+    // Canvasのコンテキスト(0, 0)にImageDataを描画
+    ctx.putImageData(imgData, 0, 0);
+  } //onload close
 }, false);
 
 download.addEventListener('click', function() {
-
   CanvasDataDownload('outputCanvas', 'output');
 }, false);
+
+// https://kuroeveryday.blogspot.com/2019/01/finding-nearest-colors-using-euclidean-distance.html
+
+function convertToRGB(hex) {
+  const match = hex.match(/[0-9a-f]{2}/gi);
+  return {
+    r: parseInt(match[0], 16),
+    g: parseInt(match[1], 16),
+    b: parseInt(match[2], 16)
+  }
+}
+
+function calcDelta(t, p) {
+  // https://en.wikipedia.org/wiki/Relative_luminance
+  // 輝度の重み付けはCIEの資料確認
+  //  0.3:0.5:0.11
+  return Math.pow((p.r - t.r), 2) +
+    Math.pow((p.g - t.g), 2) +
+    Math.pow((p.b - t.b), 2);
+}
+
+function chooseColor(palettes, rgb) {
+  let color;
+  let d = Number.MAX_SAFE_INTEGER;
+  palettes.forEach(phex => {
+    const p = convertToRGB(phex);
+    const _d = calcDelta(rgb, p);
+    if (_d < d) {
+      color = phex;
+      d = _d;
+    }
+  });
+
+  return color;
+}
